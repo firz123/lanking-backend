629b4a65db44e2f2e68bbc18f3b9e0adb4b4fc9b
Imaani Choudhuri <imaanirc@gmail.com>
2019-06-13 17:18:26 -0700

    Registered models in admin.py for easy manual testing

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Users, Landlords, Ratings, Properties
+
+admin.site.register(Users)
+admin.site.register(Landlords)
+admin.site.register(Ratings)
+admin.site.register(Properties)
