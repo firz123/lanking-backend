0adff591fa96f02d8988ef53d3937b095a00e5dd
Imaani Choudhuri <imaanirc@gmail.com>
2019-06-13 19:09:29 -0700

    Fixed typo

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-class UserView(generics.ListAPIView):
+class UserView(APIView):
 	"""
 	Create users, list all users (for testing purposes)
 	"""
